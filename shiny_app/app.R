5b520517e04edf99f274832c8a7c8707a1e10a07
ianhussey <ian.hussey@ugent.be>
2020-03-04 01:57:19 +0100

    moved functions to github urls

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -9,109 +9,9 @@ library(patchwork)
 library(psych)
 library(lavaan)
 
-# use CFA to find factor loadings and errors
-loadings_and_errors <- function(data) {
-  
-  require(tidyverse)
-  require(lavaan)
-  require(semPlot)
-  
-  # model specification
-  model <- paste("L =~", paste(colnames(data), collapse = ' + '))
-  
-  # fit model
-  fit <- cfa(model, 
-             data = data) 
-  
-  # extract estimates
-  estimates <- as.data.frame(fit@ParTable) %>%
-    select(lhs, op, rhs, est, se) %>%
-    filter(!(op == "~~" & lhs == "L" & rhs == "L")) %>%
-    rename(item = rhs,
-           estimate = est) %>%
-    mutate(ci_lower = estimate - se*1.96,
-           ci_upper = estimate + se*1.96,
-           type = ifelse(op == "=~", "factor_loading", 
-                         ifelse(op == "~~", "error", NA))) %>%
-    select(item, type, estimate, ci_lower, ci_upper) 
-  
-  plot <- 
-    estimates %>%
-    mutate(type = dplyr::recode(type, 
-                                `error` = "Errors",
-                                `factor_loading` = "Factor loadings"),
-           type = fct_relevel(type, "Factor loadings", "Errors")) %>%
-    ggplot(aes(item, estimate)) +
-    geom_point() +
-    geom_linerange(aes(ymin = ci_lower, ymax = ci_upper)) +
-    facet_wrap(~type, 
-               scales = "free", 
-               nrow = 2, 
-               ncol = 1) +
-    coord_flip() +
-    ylab("Estimate") +
-    xlab("Item")
-  
-  return(results = list(estimates = estimates,
-                        model_fit = fit, 
-                        plot = plot))
-}
-
-# pearson's r with 95% CIs
-r_with_cis <- function(data){
-  
-  require(tidyverse)
-  require(psych)
-  
-  temp <- data %>%
-    select(sum_score, score) %>%
-    cor.ci(x = ,
-           use = "pairwise.complete.obs", 
-           plot = FALSE, 
-           n.iter = 5000)
-  
-  results <- temp$ci %>%
-    select(low.e, up.e) %>%
-    mutate(r = temp$means)
-  
-  return(results)
-  
-}
-
-# attentution correction for each item with 95% CIs
-max_item_alpha <- function(data) {
-  
-  require(psych)
-  require(tidyverse)
-  
-  ic_alpha <- psych::alpha(data,
-                           n.iter = 5000)
-  
-  estimates <- data %>%
-    mutate(sum_score = rowSums(.)) %>%
-    gather(item, score, c(-sum_score)) %>%
-    group_by(item) %>%
-    do(r_with_cis(.)) %>%
-    ungroup() %>%
-    mutate(estimate = r^2 / ic_alpha$boot.ci[2],
-           ci_lower = low.e^2 / ic_alpha$boot.ci[1],
-           ci_upper = up.e^2 / ic_alpha$boot.ci[3]) %>%
-    mutate_if(is.numeric, funs(round(., 2))) %>%
-    select(-low.e, -up.e, -r)
-  
-  plot <- 
-    ggplot(estimates, aes(item, estimate)) +
-    geom_point() +
-    geom_linerange(aes(ymin = ci_lower, ymax = ci_upper)) +
-    coord_flip() +
-    ylab(expression(alpha)) +
-    xlab("Item") 
-  
-  return(results = list(estimates = estimates,
-                        plot = plot))
-  
-}
-
+source("https://raw.githubusercontent.com/ianhussey/itemselectr/master/R/loadings_and_errors.r")
+source("https://raw.githubusercontent.com/ianhussey/itemselectr/master/R/max_item_alpha.r")
+source("https://raw.githubusercontent.com/ianhussey/itemselectr/master/R/r_with_cis.r")
 
 # options ----
 
@@ -150,7 +50,7 @@ ui <-
                           # Horizontal line ----
                           tags$hr(),
                           
-                          tags$b("CSV files properties"),
+                          tags$b("CSV file properties"),
                           
                           # Input: Checkbox if file has header ----
                           checkboxInput("header", "Header", TRUE),
